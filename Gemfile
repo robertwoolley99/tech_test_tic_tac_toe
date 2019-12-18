154341f49704bb6f6670e00cf7f8692d25fff75a
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-18 17:12:54 +0000

    Cleaned up board; added rubocop and simplecov

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+gem 'rubocop', require: false
+
+gem 'simplecov', require: false, group: :test
--- lib/board.rb
+++ lib/board.rb
@@ -3,21 +3,26 @@
 # Class which holds data state and logic
 class Board
   def initialize
-    @grid = { 'A1'=> '', 'B1'=> '', 'C1'=> '',
-      'A2'=> '', 'B2'=> '', 'C2'=> '',
-      'A3'=> '', 'B3'=> '', 'C3'=> '' }
-    end
-    def play(player_and_location)
-      player_piece = player_and_location[0]
-      grid_location = player_and_location[1,2]
-      unless @grid.key?(grid_location) == true
-        raise 'Not a valid square. Please choose another one.'
-      end
-      if @grid[grid_location] == ''
-        @grid[grid_location] = player_piece
-        return @grid[grid_location]
-      else
-        raise 'Already played. Choose another square.'
-      end
-    end
+    @grid = { 'A1' => '', 'B1' => '', 'C1' => '',
+              'A2' => '', 'B2' => '', 'C2' => '',
+              'A3' => '', 'B3' => '', 'C3' => '' }
   end
+
+  def play(player_and_location)
+    player_piece = player_and_location[0]
+    grid_location = player_and_location[1, 2]
+    validate_move(player_piece, grid_location)
+  end
+
+  def validate_move(player_piece, grid_location)
+    unless @grid.key?(grid_location) == true
+      raise 'Not a valid square. Please choose another one.'
+    end
+
+    unless @grid[grid_location] == ''
+      raise 'Already played. Choose another square.'
+    end
+
+    @grid[grid_location] = player_piece
+  end
+end
